65bfaf12b56fa1c66887ee0d24e3ff8b28bca39c
khaidem <khaidemdenish@gmail.com>
2021-12-28 16:56:14 +0530

    finsih

--- lib/app/widget/app.widget.dart
+++ lib/app/widget/app.widget.dart
@@ -1,29 +1,38 @@
+import 'package:day20/loginLogout/data/repo/newsfeed.repo.dart';
 import 'package:day20/loginLogout/logic/cubit/authflow_cubit.dart';
+import 'package:day20/loginLogout/logic/newsfeed/cubit/newsfeed_cubit.dart';
 import 'package:day20/loginLogout/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key, required this.appRouter}) : super(key: key);
+  MyApp({
+    Key? key,
+    required this.appRouter,
+  }) : super(key: key);
 
   final AppRouter appRouter;
+  final NewsfeedRepo newsfeedRepo = NewsfeedRepo();
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AuthflowCubit(),
+    return MultiRepoWrapper(
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AuthflowCubit(),
+          ),
+          BlocProvider(create: (context) => NewsfeedCubit(repo: newsfeedRepo)),
+        ],
+        child: MaterialApp.router(
+          routerDelegate: appRouter.delegate(),
+          routeInformationParser: appRouter.defaultRouteParser(),
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          debugShowCheckedModeBanner: false,
         ),
-      ],
-      child: MaterialApp.router(
-        routerDelegate: appRouter.delegate(),
-        routeInformationParser: appRouter.defaultRouteParser(),
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        debugShowCheckedModeBanner: false,
       ),
     );
   }
